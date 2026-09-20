ff17c0f414f4fd1deacda864c30f8a61f3d9ba91
Ayush karki <riganrootsroutes@gmail.com>
2026-09-20 14:15:36 +0545

    release: satisfy Activity Result fragment requirement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
   implementation(libs.firebase.messaging)
   // implementation(libs.accompanist.permissions)
   implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.fragment.ktx)
   // implementation(libs.androidx.camera.camera2)
   // implementation(libs.androidx.camera.core)
   // implementation(libs.androidx.camera.lifecycle)
